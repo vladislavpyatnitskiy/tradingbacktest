b0cef31342ecf637ab635c0d3b0ff90031bbd2a2
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-11-20 13:09:30 +0500

    Update Regression Data Frame.R
    Optimised script:
    
    • Reduced excessive lines
    • Added and edited comments

--- Statmethods/Regression Data Frame.R
+++ Statmethods/Regression Data Frame.R
@@ -1,8 +1,8 @@
 library(timeSeries) # Library
 
-reg.df <- function(x, y){ # Actual Data Frame vs Expected by Regression
+reg.df <- function(x, y){ # Multiply Regression values with data frame 
   
-  D <- y[,1]
+  D <- y[,1] # Subset first column from main data set
   
   x <- as.data.frame(x$coefficients[,1]) # Regression coefficients
   
@@ -12,35 +12,32 @@ reg.df <- function(x, y){ # Actual Data Frame vs Expected by Regression
   
   x <- as.data.frame(x[-1,]) # Reduce first column
   
-  a <- rownames(y)
+  a <- rownames(y) # Subset row names with dates
+  
   rownames(x) <- r # Change row names to one without first row name
   
-  L <- NULL
+  L <- NULL # Make data frame of observations to group of vectors
   
   for (n in 1:nrow(y)){ s <- t(as.data.frame(as.data.frame(y[n,])[,-1])) 
   
-    s <- as.data.frame(s[order(row.names(s)), ])
+    L <- c(L, s <- as.data.frame(s[order(row.names(s)), ])) } 
     
-    L <- c(L, s) } # Sum with intercept
-    
-  L <- list(L)
+  L <- list(L) # Convert group of vectors to nested list
   
-  H <- NULL
+  H <- NULL # Create Data Frame of Expected Values from Regression output
   
   for (m in 1:length(L[[1]])){ d <- data.frame(x, as.data.frame(L[[1]][m]))
   
-    d$var <- d[,1] * d[,2] # Sum Product of two columns
+    d$var <- d[,1] * d[,2] # Multiply column values
     
-    H <- rbind.data.frame(H, sum(d[,3]) + g) }
+    H <- rbind.data.frame(H, sum(d[,3]) + g) } # Sum intercept with products
     
-  rownames(H) <- a
+  rownames(H) <- a # Assign dates as row names
   
-  H <- as.timeSeries(H)
-  
-  df <- cbind.data.frame(D, H)
+  df <- cbind.data.frame(D, as.timeSeries(H)) # Join
   
   colnames(df) <- c(sprintf("Actual %s", colnames(D)),
-                    sprintf("Expected %s", colnames(D)))
+                    sprintf("Expected %s", colnames(D))) # Change column names
   as.timeSeries(df)
 }
 reg.df(df.for.backtest, backtest.df) # Test
